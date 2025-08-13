9faf72f329ae405a455f8e893a6c0f64100e7041
vyshnavi <vvyhu17@gmail.com>
2025-08-13 21:22:24 +0530

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,3 +1,5 @@
+//LINEAR SEARCH
+
 #include <stdio.h>
 
 
@@ -28,3 +30,41 @@ int main() {
     printf("%d",ans);
     return 0;
 }
+
+
+//Binary Search
+
+#include <stdio.h>
+
+int bs(int arr[], int beg, int end, int key) {
+    if (beg <= end) {
+        int mid = (beg + end) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (key < arr[mid])
+            return bs(arr, beg, mid - 1, key);
+        else
+            return bs(arr, mid + 1, end, key);
+    }
+    return -1;
+}
+
+int main() {
+    int n, key;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements in sorted order:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+    int result = bs(arr, 0, n - 1, key);
+    if (result != -1)
+        printf("Element found at index %d\n", result);
+    else
+        printf("Element not found\n");
+    return 0;
+}
+
